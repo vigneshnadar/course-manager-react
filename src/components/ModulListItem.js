a848e088dc2d957169f1497649e7ee1ce3ae1cf9
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-05-18 17:12:57 -0400

    Module list item component

---
+++ src/components/ModulListItem.js
@@ -0,0 +1,13 @@
+import React,{ Component } from 'react';
+
+class ModuleListItem extends Component {
+
+    render(){
+        return  (<li className="list-group-item">
+                    Module 1
+                    </li>
+            );
+    }
+}
+
+export default ModuleListItem;
--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -1,5 +1,5 @@
 import React,{ Component } from 'react';
-import CourseCard  from './CourseCard';
+import CourseCard  from '../components/CourseCard';
 import 'bootstrap/dist/css/bootstrap.css';
 
 class CourseManager extends Component {
