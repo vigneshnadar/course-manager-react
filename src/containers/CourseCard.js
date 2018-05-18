2bc6066b977d1cffcafdfde759cf979f1edb9a0d
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-05-18 12:29:32 -0400

    course card styling

---
+++ src/containers/CourseCard.js
@@ -0,0 +1,18 @@
+import React,{ Component } from 'react';
+
+class CourseCard extends Component {
+
+    render(){
+        return  (
+            <div className="card" styles={{width: '18rem'}}>
+                <img className="card-img-top"
+                     src="https://picsum.photos/300/200"/>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Card text.</p>
+                    <a href="#"><button className="btn btn-primary">More...</button></a>
+                </div></div>)
+    }
+}
+
+export default CourseCard;
--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -1,9 +1,17 @@
 import React,{ Component } from 'react';
+import CourseCard  from './CourseCard';
 
 class CourseManager extends Component {
 
     render(){
-        return <div className="container-fluid"><h1>Course Manager</h1></div>
+        return <div className="container-fluid">
+            <h1>Course Manager</h1>
+            <div className="card-deck">
+            <CourseCard/>
+            <CourseCard/>
+            <CourseCard/>
+            </div>
+        </div>
     }
 }
 
