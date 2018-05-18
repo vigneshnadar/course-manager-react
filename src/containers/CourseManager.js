46de792bad141e7798423515669af12cfc4f69f4
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-05-18 12:13:54 -0400

    course manager

---
+++ src/containers/CourseManager.js
@@ -0,0 +1,10 @@
+import React,{ Component } from 'react';
+
+class CourseManager extends Component {
+
+    render(){
+        return <div className="container-fluid"><h1>Course Manager</h1></div>
+    }
+}
+
+export default CourseManager;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Helloworld from './hello';
+import CourseManager from './containers/CourseManager';
 
-ReactDOM.render(<Helloworld/>,
+ReactDOM.render(<CourseManager/>,
     document.getElementById('root'));
