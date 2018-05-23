6e52b1669814cc432b4539c5605834c2941f1b29
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-05-23 06:07:35 -0400

    bind course title change event

--- src/containers/CourseList.js
+++ src/containers/CourseList.js
@@ -8,7 +8,12 @@ class CourseList extends React.Component {
     constructor() {
         super();
         this.courseService = CourseService.instance;
-        this.state = {courses : []};
+        this.state = {
+            course : {title : ''},
+            courses : []};
+
+        this.titleChanged = this.titleChanged.bind(this);
+        this.createModule = this.createModule.bind(this);
     }
 
 
@@ -18,6 +23,18 @@ class CourseList extends React.Component {
                 console.log(courses);
                 this.setState({ courses : courses})
             });
+    }
+
+
+    titleChanged(event) {
+        this.setState({course: {title: event.target.value}});
+        console.log(this.state);
+        console.log(event.target.value);
+    }
+
+    createModule(event) {
+        this.setState({course: {title: event.target.value}});
+        console.log(event.target.value);
     }
 
 
@@ -47,7 +64,14 @@ class CourseList extends React.Component {
             <div>
             <h2>Course List</h2>
             <table className="table">
-            <thead><tr><th>Title</th></tr></thead>
+            <thead>
+            <tr><th>Title</th></tr>
+            <tr>
+                <th><input onChange={this.titleChanged} className="form-control" id="titleFld"
+                           placeholder="cs101"/></th>
+                <th><button onChange={this.createModule} className="btn btn-primary">Add</button></th>
+            </tr>
+            </thead>
         <tbody>
         {/*<tr><td>Course Row</td></tr>*/}
         {/*<CourseRow/>*/}
