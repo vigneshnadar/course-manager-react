2eaf3abd49432e4aa59adbca379108569cc0d8e0
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-05-23 09:39:41 -0400

    course edit dynamically via route

--- src/containers/CourseEditor.js
+++ src/containers/CourseEditor.js
@@ -3,7 +3,26 @@ import ModuleList from './ModuleList';
 import LessonTab from './LessonTab';
 
 class CourseEditor extends React.Component {
-    render() { return (<div className="row">
+
+    constructor(props){
+        super(props);
+        this.state = {courseId : ''};
+        this.selectCourse = this.selectCourse.bind(this);
+    }
+
+    componentDidMount(){
+        this.selectCourse(this.props.match.params.courseId);
+    }
+
+
+    selectCourse(courseId){
+        this.setState({courseId : courseId });
+    }
+
+    render() { return (
+        <div>
+            <h2> Editing Course: {this.state.courseId} </h2>
+        <div className="row">
         <div className="col-4">
             <ModuleList/>
         </div>
@@ -11,6 +30,7 @@ class CourseEditor extends React.Component {
             <LessonTab/>
         </div>
     </div>
+        </div>
     );}}
 
 export default CourseEditor;
